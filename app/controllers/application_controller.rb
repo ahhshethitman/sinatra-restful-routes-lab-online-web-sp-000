dd2e0b21765d45012bafb3a05660fb3ec2692a05
Hunter Sides <hunter.s.1337@gmail.com>
2020-10-29 16:43:37 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -39,9 +39,11 @@ class ApplicationController < Sinatra::Base
   end
 
   delete '/recipes/:id/delete' do
-    recipe = Recipe.find_by_id(params[:id])
-    recipe.delete
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.delete
     redirect to '/recipes'
   end
+  
+
 
 end
